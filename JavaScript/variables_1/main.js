0eea49aaf06fa2c6f0b4ce4e1997f2645ae619c0
ecki-d <support@kaizenmedia.co.za>
2018-11-22 13:45:58 +0200

    JS

---
+++ JavaScript/variables_1/main.js
@@ -0,0 +1,44 @@
+// Variables 
+let human = 'Jesse'; // String
+human = 'Eckhardt'; // Change variable
+const myDog = 'Zoe';
+const myNewDog = 'Jeff';
+
+// Practice
+let myCarIsCrashed = "crashed"; 
+let myCarIsNotCrashed = "not crashed";
+
+
+// More variables
+const name = 'Jesse';
+let age = 22;
+let birthday = false;
+
+if(birthday === true) { ////////
+  age = age + 1;
+}
+
+// Birthday changes
+birthday = true;
+
+if(birthday === true) { ///////////
+  age = age + 1;
+}
+
+// Maak twee vriende met name
+// log uit naam1: "hello! naam2"
+// naam2: hello! naam1
+const human1 = 'Jesse';
+const human2 = "Eckhart";
+console.log(human1 +": hallo "+ human2);
+
+"Jesse" + "eckhardt"
+
+/* key notes:
+  - Maak variables
+  - Gestoor op computer
+  - Hulle het unique name
+  - Kan verander
+  - Constants kan nie verander (gebruik as jy weet dit bly die selfde)
+*/
+
